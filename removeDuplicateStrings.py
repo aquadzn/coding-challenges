4e6b06b2e61869d0e595bd3a373c8d96fb508ab2
aquadzn <williamjcqs8@gmail.com>
2020-01-02 11:50:31 +0100

    Add removeDuplicateStrings

---
+++ removeDuplicateStrings.py
@@ -0,0 +1,9 @@
+# def removeDuplicateStrings(a):
+#     return list(OrderedDict.fromkeys(a))
+# 64 
+
+# removeDuplicateStrings = lambda a: list(OrderedDict.fromkeys(a))
+# 60
+
+return list(OrderedDict.fromkeys(*eval(dir()[0])))
+# 49
