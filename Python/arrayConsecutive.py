efa29dd054d15f623b52baa30f111be5a5aa6be6
William Jacques <46140458+aquadzn@users.noreply.github.com>
2020-01-07 00:02:00 +0100

    125 chars

--- Python/arrayConsecutive.py
+++ Python/arrayConsecutive.py
@@ -5,9 +5,11 @@ s = 0
 
 while len(L) > 1:
     if s % 2 == 0:
-        L = [a + b for a, b in zip(L[:-1:2], L[1::2])]
+        L = numpy.add(L[:-1:2], L[1::2])
+        # L = [a + b for a, b in zip(L[:-1:2], L[1::2])]
     else:
-        L = [a * b for a, b in zip(L[:-1:2], L[1::2])]
+        L = numpy.multiply(L[:-1:2], L[1::2])
+        # L = [a * b for a, b in zip(L[:-1:2], L[1::2])]
     s += 1
 
 return L[0]
