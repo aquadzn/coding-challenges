f0b328fbd080f983fdbdcc5fb3381727174731ad
William Jacques <46140458+aquadzn@users.noreply.github.com>
2020-02-17 11:18:25 +0100

    Better solution

--- Python/pressingButtons.py
+++ Python/pressingButtons.py
@@ -3,6 +3,5 @@ import itertools as i
 # *re.findall('...', 'abcdefghijklmno')
 # >>> 'abc', 'def', 'ghi', 'jkl', 'mno'
 
-return ["".join(s) for s in i.product(*[[0,0,"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"][int(i)] for i in eval(dir()[0])[0]]) if s]
-
-# 145
+# [0,0,"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"] 
+return ["".join(s) for s in i.product(*['0 0 abc def ghi jkl mno pqrs tuv wxyz'.split()[int(i)] for i in eval(dir()[0])[0]]) if s]
